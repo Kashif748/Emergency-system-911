c217a3bda3658c2620a73254aea3795705271abc
AbdelHady Anka <abdelhady.anka@gmail.com>
2023-02-09 12:47:53 +0400

    Product Backlog Item #467: Tasks Dashboard - Revamp Module FrontEnd

---
+++ src/app/shared/sh-directives/due-date.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { DueDateDirective } from './due-date.directive';
+
+describe('Directive: DueDate', () => {
+  it('should create an instance', () => {
+    const directive = new DueDateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/shared/sh-pipes/cdate.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CdatePipe } from './cdate.pipe';
+
+describe('Pipe: Cdatee', () => {
+  it('create an instance', () => {
+    let pipe = new CdatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
