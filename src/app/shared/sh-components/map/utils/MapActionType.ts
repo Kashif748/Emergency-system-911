737ee96d42888b633da3ff56af8dc16d4efea82d
AbdelHady Anka <abdelhady.anka@gmail.com>
2023-02-22 08:36:15 +0400

    add seperated map module

---
+++ src/app/shared/sh-components/map/utils/MapActionType.ts
@@ -0,0 +1,10 @@
+export enum MapActionType {
+  INCIDENT_POINT = 'Inc_point',
+  INCIDENT_POLYLINE = 'Inc_Polyline',
+  INCIDENT_POLYGON = 'Inc_Polygon',
+  TASK_POINT = 'Tsk_point',
+  TASK_POLYLINE = 'Tsk_Polyline',
+  TASK_POLYGON = 'Tsk_Polygon',
+  ORGRANIZATION_POINT = 'Org_Point',
+  ASSET_POINT = 'Asset_Point'
+}
