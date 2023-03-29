f9268b6dcaba10b90c65e7bab48d8fbd37e25f60
sameh <samehsy16@gmail.com>
2023-03-29 13:29:59 +0300

    localization

--- src/app/modules/_business-continuity/business-continuity-routing.module.ts
+++ src/app/modules/_business-continuity/business-continuity-routing.module.ts
@@ -9,12 +9,13 @@ import { LocTypeComponent } from './loc-type/loc-type.component';
 import { OrgDetailsComponent } from './org-details/org-details.component';
 import { OrgStrucureComponent } from './org-strucure/org-strucure.component';
 import { RtoListContentComponent } from './rto-list-content/rto-list-content.component';
-import {ImpLevelWorkingComponent} from "./imp-level-working/imp-level-working.component";
+import { ImpLevelWorkingComponent } from './imp-level-working/imp-level-working.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BusinessContinuityComponent,
+
     children: [
       {
         path: 'org-details',
@@ -51,7 +52,7 @@ const routes: Routes = [
       {
         path: 'imp-level-working',
         component: ImpLevelWorkingComponent,
-      }
+      },
     ],
   },
 ];
