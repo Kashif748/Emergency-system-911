4f373f69c4e31b948424dac55c93bd4f3cb28ab7
sameh <samehsy16@gmail.com>
2023-07-06 23:45:46 +0300

    locations emplementaion

--- src/app/modules/_business-continuity-setup/location/browse-location/location-content/location-content.component.ts
+++ src/app/modules/_business-continuity-setup/location/browse-location/location-content/location-content.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {LOCATIONS} from "../../../tempData.conts";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
 import {Store} from "@ngxs/store";
+import { BcLocations } from 'src/app/api/models';
+import { PageRequestModel } from '@core/models/page-request.model';
+import { LazyLoadEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-location-content',
@@ -9,15 +11,33 @@ import {Store} from "@ngxs/store";
   styleUrls: ['./location-content.component.scss']
 })
 export class LocationContentComponent implements OnInit {
-  public page = LOCATIONS;
-  public loading = false;
-  public columns: string[] = [ 'criticality', 'rto' , 'desc' ];
+  @Input()
+  loading: boolean;
+  @Input()
+  page: BcLocations[];
+  @Input()
+  columns: string[];
+  @Input()
+  totalRecords: number;
+  @Input()
+  pageRequest: PageRequestModel;
+
+  @Output()
+  onPageChange = new EventEmitter<LazyLoadEvent>();
+
+  public display = false;
+  // public loading = false;
+  // public columns: string[] = [ 'criticality', 'rto' , 'desc' ];
   constructor(
     private store: Store,
     private translate: TranslateService,
   ) { }
 
   ngOnInit(): void {
+    this.onPageChange.emit({
+      first: this.pageRequest?.first,
+      rows: this.pageRequest?.rows,
+    });
   }
 
 }
---
+++ src/app/modules/_business-continuity-setup/location/states/browse-locations.action.ts
@@ -0,0 +1,64 @@
+import { PageRequestModel } from '@core/models/page-request.model';
+import { BcLocations } from 'src/app/api/models';
+
+export namespace BrowseLocationsAction {
+  export class LoadLocations {
+    static readonly type = '[BrowseLocations] Load Locations';
+
+    /**
+     *
+     */
+    constructor(public payload?: { pageRequest?: PageRequestModel }) {}
+  }
+
+  export class GetLocation {
+    static readonly type = '[BrowseLocations] Get Location';
+    /**
+     *
+     */
+    constructor(
+      public payload: {
+        id?: number;
+      }
+    ) {}
+  }
+  export class CreateLocation {
+    static readonly type = '[BrowseLocations] Create Location';
+    /**
+     *
+     */
+    constructor(public payload: BcLocations) {}
+  }
+
+  export class UpdateLocation {
+    static readonly type = '[BrowseLocations] Update Location';
+    /**
+     *
+     */
+    constructor(public payload: BcLocations) {}
+  }
+
+  export class ToggleDialog {
+    static readonly type = '[BrowseLocations] Toggle Dialog';
+    /**
+     *
+     */
+    constructor(public payload: { locationId?: number }) {}
+  }
+
+  export class OpenView {
+    static readonly type = '[BrowseLocations] Open View';
+    /**
+     *
+     */
+    constructor(public payload: { locationId: number }) {}
+  }
+
+  export class ChangeColumns {
+    static readonly type = '[BrowseLocations] Change Columns';
+    /**
+     *
+     */
+    constructor(public payload: { columns: string[] }) {}
+  }
+}
