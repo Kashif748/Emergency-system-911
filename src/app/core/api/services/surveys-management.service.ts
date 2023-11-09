f1f7aa5ac6a2b4afb769399f2c56fea61df7f5fd
sameh <samehsy16@gmail.com>
2023-11-09 09:03:21 +0400

    export filter

--- src/app/core/api/services/surveys-management.service.ts
+++ src/app/core/api/services/surveys-management.service.ts
@@ -139,6 +139,7 @@ export class SurveysManagementService {
           happiness: filterForm?.happiness ?? '',
           reason: filterForm?.reason ?? '',
           incidentId: filterForm?.incident ?? '',
+          incidentCategoryId: filterForm?.category ?? '',
         },
 
         responseType: 'blob' as any,
@@ -216,7 +217,7 @@ export class SurveysManagementService {
             fromDate: filterForm?.fromDate ?? '',
             toDate: filterForm?.toDate ?? '',
             incidentId: filterForm?.incident ?? '',
-            incidentCategoryId: filterForm?.category ?? '',
+            // incidentCategoryId: filterForm?.category ?? '',
             sort: `${sort?.active ?? ''},${
               sort?.direction ?? 'createdDate,desc'
             }`,
@@ -256,6 +257,7 @@ export class SurveysManagementService {
           fromDate: filterForm?.fromDate ?? '',
           toDate: filterForm?.toDate ?? '',
           incidentId: filterForm?.incident ?? '',
+          // incidentCategoryId: filterForm?.category ?? '',
         },
 
         responseType: 'blob' as any,
